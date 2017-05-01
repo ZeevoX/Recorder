28ad3d32c3df4ab6cfb0d341df9053ec7b5441d6
ZeevoX <langer.timothy@gmail.com>
2017-05-01 10:17:58 +0100

    INIT

---
+++ app/src/main/java/com/zeevox/recorder/RecorderActivity.java
@@ -0,0 +1,122 @@
+package com.zeevox.recorder;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+
+public class RecorderActivity extends AppCompatActivity {
+
+    protected SharedPreferences preferences;
+    private String KEY_THEME = "key_app_theme";
+    private boolean mDarkTheme;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        this.mDarkTheme = this.preferences.getBoolean(KEY_THEME, false);
+        int i;
+        if (this.mDarkTheme) {
+            i = R.style.AppThemeBlack;
+        } else {
+            i = R.style.AppTheme;
+        }
+        setTheme(i);
+
+        setContentView(R.layout.activity_recorder);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
+
+        final FABToolbarLayout layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabtoolbar_fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startRecording(1, 44100, 96000, Environment.getExternalStorageDirectory() + File.separator + "Recordings" + File.separator + "Recording-1");
+                dialogtemporarydeletesoon();
+                layout.show();
+
+
+                final ImageView stop = (ImageView) findViewById(R.id.recording_stop);
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //stopRecording();
+                        layout.hide();
+                    }
+                });
+            }
+        });
+    }
+
+    private void dialogtemporarydeletesoon() {
+        new MaterialDialog.Builder(this)
+                .title("Information")
+                .content("It is not possible to record in this application build. Sorry for any inconvenience caused.")
+                .positiveText("OK")
+                .show();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_theme).setTitle(this.mDarkTheme ? R.string.action_theme_light : R.string.action_theme_dark);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recorder, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_theme:
+                this.mDarkTheme = !this.mDarkTheme;
+                this.preferences.edit().putBoolean(KEY_THEME, this.mDarkTheme).apply();
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(new Intent(this, getClass()));
+                overridePendingTransition(0, 0);
+                return false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void startRecording(int audioChannels, int samplingRate, int encodingBitRate, String recordingFilePath) {
+
+        //init
+        MediaRecorder recorder = new MediaRecorder();
+
+        //Audio source
+        recorder.setAudioChannels(audioChannels);
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setAudioEncoder(MediaRecorder.getAudioSourceMax());
+        recorder.setAudioSamplingRate(samplingRate);
+        recorder.setAudioEncodingBitRate(encodingBitRate);
+        recorder.setOutputFile(recordingFilePath);
+    }
+}
