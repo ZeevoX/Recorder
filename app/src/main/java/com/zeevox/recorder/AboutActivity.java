b05dd88078dfb031f97b6d9ae99f340e9f80d5c4
ZeevoX <langer.timothy@gmail.com>
2017-07-22 16:47:58 +0200

    Cleanup AboutActivity

--- app/src/main/java/com/zeevox/recorder/AboutActivity.java
+++ app/src/main/java/com/zeevox/recorder/AboutActivity.java
@@ -17,28 +17,8 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        setTheme(preferences.getBoolean("key_app_theme", false) ? R.style.AppThemeBlack : R.style.AppTheme);
-
-        setContentView(R.layout.activity_about);
-
-        final ImageView recorderIcon = findViewById(R.id.aboutRecorderIcon);
-        recorderIcon.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                vibrator.vibrate(30);
-                if (i == 7) {
-                    startActivity(new Intent(AboutActivity.this, FeedbackActivity.class));
-                    i = 0;
-                } else {
-                    i = i+1;
-                }
-                return false;
-            }
-        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
         ((TextView) findViewById(R.id.textTimeStamp)).setText(String.format("%s%s", new Object[]{getString(R.string.about_version_placeholder), BuildConfig.VERSION_NAME}));
     }
 
