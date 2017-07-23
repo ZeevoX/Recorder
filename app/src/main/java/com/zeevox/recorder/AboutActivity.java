f65c8f6c8827612080ccc4805621a9fef16c3a3f
ZeevoX <langer.timothy@gmail.com>
2017-07-23 11:44:33 +0200

    Cleanup AboutActivity

--- app/src/main/java/com/zeevox/recorder/AboutActivity.java
+++ app/src/main/java/com/zeevox/recorder/AboutActivity.java
@@ -1,25 +1,18 @@
 package com.zeevox.recorder;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Vibrator;
-import android.preference.PreferenceManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-
-    private int i;
-
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        ((TextView) findViewById(R.id.textTimeStamp)).setText(String.format("%s%s", new Object[]{getString(R.string.about_version_placeholder), BuildConfig.VERSION_NAME}));
+        //((TextView) findViewById(R.id.textTimeStamp)).setText(String.format("%s%s", new Object[]{getString(R.string.about_version_placeholder), BuildConfig.VERSION_NAME}));
     }
 
     /*
