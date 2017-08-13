9133a663f40d1557f55181883253a9d5b49773c5
Timothy Langer <langer.timothy@gmail.com>
2017-08-13 22:02:31 +0100

    Apply Google Java format on 'audio' package

--- app/src/main/java/com/zeevox/recorder/audio/Disposable.java
+++ app/src/main/java/com/zeevox/recorder/audio/Disposable.java
@@ -1,8 +1,11 @@
 package com.zeevox.recorder.audio;
 
-/** Interface for disposable resources.
- * @author mzechner */
+/**
+ * Interface for disposable resources.
+ *
+ * @author mzechner
+ */
 public interface Disposable {
-    /** Releases all resources of this object. */
-    public void dispose ();
+  /** Releases all resources of this object. */
+  public void dispose();
 }
